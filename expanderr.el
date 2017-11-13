a4ef3f437544d5f669ff9b88885dbb3013fa0e34
Thomas Frössman <thomasf@jossystem.se>
2017-11-13 13:43:18 +0100

    fix relative load

--- expanderr.el
+++ expanderr.el
@@ -1,4 +1,4 @@
 ;; this file provides backwards compatibilty with previously documented way of
 ;; loading this libray in the readme:
 ;; (load "~/go/src/github.com/stapelberg/expanderr/expanderr.el")
-(load "lisp/go-expanderr.el")
+(load-file (expand-file-name "lisp/go-expanderr.el" (file-name-directory load-file-name)))
